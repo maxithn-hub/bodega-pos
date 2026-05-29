489666de66df2842ca17fd2577d5263596ace03d
maxithn-hub <maxithn-hub@users.noreply.github.com>
2026-05-29 17:35:47 -0400

    feat: Implement absolute domain lock security and license verification for Web Demo

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2051616896" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2418860101" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
